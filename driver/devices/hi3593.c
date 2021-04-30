37b806e73121683e85d56467d436e68ae1ca3471
charles <charles@ccxtechnologies.com>
2021-04-30 14:03:06 -0400

    minor code cleanup

--- driver/devices/hi3593.c
+++ driver/devices/hi3593.c
@@ -749,7 +749,7 @@ static void hi3593_tx_worker(struct work_struct *work)
 	struct hi3593_priv *priv;
 	struct sk_buff *skb;
 	avionics_data *data;
-	__u8 rd_cmd, wr_cmd[5], send_cmd;
+	__u8 rd_cmd, wr_cmd[5];
 	__u32 vbuffer;
 	__u64 time_msecs, offset_msecs;
 	ssize_t status;
