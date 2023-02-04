2bc1123f6c18c40e1e961d51d8af5fefa679862e
charles <charles@ccxtechnologies.com>
2023-02-04 11:25:39 -0500

    add config set tool architecture, not finished

--- tools/avionics-config-set.py
+++ tools/avionics-config-set.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python
-# Copyright: 2019-2021, CCX Technologies
+# Copyright: 2023, CCX Technologies
 
-import socket
-import struct
-import fcntl
-import collections
-
+import sys
 import os
 
-AF_AVIONICS = 18
-PF_AVIONICS = 18
-AVIONICS_RAW = 1
-AVIONICS_TIMESTAMP = 2
+import socket
+import ctypes
+import struct
+import fcntl
 
-SIOCGIFINDEX = 0x8933
-
-# from include/uapi/linux/rtnetlink.h.
+# ===== from linux/rtnetlink.h ========
 RTM_NEWLINK = 16
 RTM_GETLINK = 18
 
-# from include/uapi/linux/netlink.h.
+
+class ifinfomsg(ctypes.Structure):
+    _fields_ = [
+            ('ifi_family', ctypes.c_ubyte),
+            ('__ifi_pad', ctypes.c_ubyte),
+            ('ifi_type', ctypes.c_ushort),
+            ('ifi_index', ctypes.c_int),
+            ('ifi_flags', ctypes.c_uint),
+            ('ifi_change', ctypes.c_uint),
+    ]
+
+
+class rtattr(ctypes.Structure):
+    _fields_ = [
+            ('rta_len', ctypes.c_ushort),
+            ('rta_type', ctypes.c_ushort),
+    ]
+
+
+# ===== from linux/netlink.h ==========
+
 NLM_F_REQUEST = 1
 NLM_F_ACK = 4
 
@@ -28,16 +42,49 @@ NLMSG_DONE = 3
 
 IFLA_LINKINFO = 18
 
-# from include/uapi/linux/if_link.h
+
+class nlmsghdr(ctypes.Structure):
+    _fields_ = [
+            ('nlmsg_len', ctypes.c_uint32),
+            ('nlmsg_type', ctypes.c_uint16),
+            ('nlmsg_flags', ctypes.c_uint16),
+            ('nlmsg_seq', ctypes.c_uint32),
+            ('nlmsg_pid', ctypes.c_uint32),
+    ]
+
+
+class nlmsgerr(ctypes.Structure):
+    _fields_ = [
+            ('error', ctypes.c_int),
+            ('msg', nlmsghdr),
+    ]
+
+
+# ===== from linux/if_link.h ==========
+
 IFLA_INFO_KIND = 1
 IFLA_INFO_DATA = 2
 
-# from avionics.h
-IFLA_AVIONICS_RATE = 1
-IFLA_AVIONICS_ARINC429RX = 2
-IFLA_AVIONICS_ARINC429TX = 3
-IFLA_AVIONICS_ARINC717RX = 4
-IFLA_AVIONICS_ARINC717TX = 5
+# ===== from linux/if.h ===============
+
+IFF_UP = 1
+
+# ===== from linux/socket.h ===========
+
+AF_ASH = 18
+
+# ===== from linux/sockios.h ==========
+
+SIOCGIFINDEX = 0x8933
+
+# ===== from avionics.h ===============
+
+AF_AVIONICS = AF_ASH
+PF_AVIONICS = AF_AVIONICS
+
+AVIONICS_PROTO_RAW = 1
+AVIONICS_PROTO_TIMESTAMP = 2
+AVIONICS_PROTO_PACKET = 3
 
 AVIONICS_ARINC429RX_FLIP_LABEL_BITS = (1 << 7)
 AVIONICS_ARINC429RX_SD9_MASK = (1 << 6)
@@ -60,353 +107,212 @@ AVIONICS_ARINC717RX_SFTSYNC = (1 << 2)
 AVIONICS_ARINC717TX_SLEW = (3 << 1)
 AVIONICS_ARINC717TX_SELF_TEST = (1 << 0)
 
-# from include/uapi/linux/if.h
-IFF_UP = 1
+IFLA_AVIONICS_RATE = 1
+IFLA_AVIONICS_ARINC429RX = 2
+IFLA_AVIONICS_ARINC429TX = 3
+IFLA_AVIONICS_ARINC717RX = 4
+IFLA_AVIONICS_ARINC717TX = 5
+IFLA_AVIONICS_MIL1553MB = 5
 
 
-def get_index(device):
-    with socket.socket(PF_AVIONICS, socket.SOCK_RAW, AVIONICS_RAW) as sock:
-        data = struct.pack("16si", device.encode(), 0)
-        res = fcntl.ioctl(sock, SIOCGIFINDEX, data)
-        index, = struct.unpack("16xi", res)
-        return index
+class avionics_rate(ctypes.Structure):
+    _fields_ = [
+            ('rate_hz', ctypes.c_uint32),
+    ]
 
 
-class CStruct:
-    def __init__(self, name, pack, fields):
-        self.struct = struct.Struct(pack)
-        self.args = collections.namedtuple(name, fields)
-
-    def __len__(self):
-        return self.struct.size
-
-    def pack(self, *args, **kwargs):
-        _args = self.args(*args, **kwargs)
-        return self.struct.pack(*_args)
-
-    def unpack(self, data):
-        return self.args._make(self.struct.unpack(data))
-
-    def consume(self, data):
-        return data[len(self):], self.unpack(data[:len(self)])
+class avionics_arinc429rx(ctypes.Structure):
+    _fields_ = [
+            ('flags', ctypes.c_uint8),
+            ('padding', ctypes.c_uint8),
+            ('priority_labels', ctypes.c_uint8 * 3),
+            ('label_filters', ctypes.c_uint8 * 32),
+    ]
 
 
-nlmsghdr = CStruct(
-        "nlmsghdr", "=LHHLL",
-        ("nlmsg_len", "nlmsg_type", "nlmsg_flags", "nlmsg_seq", "nlmsg_pid")
-)
-nlmsgerr = CStruct("nlmsgerr", "=i", ("error"))
-ifinfomsg = CStruct(
-        "ifinfomsg", "=BxHiII",
-        ("ifi_family", "ifi_type", "ifi_index", "ifi_flags", "ifi_change")
-)
-rattr = CStruct("rattr", "=HH", ("rta_len", "rta_type"))
-
-avionics_rate = CStruct("avionics_rate", "=L", ("rate_hz"))
-avionics_arinc429rx = CStruct(
-        "avionics_arinc429rx", "=Bx3s32s",
-        ("flags", "priority_labels", "label_filters")
-)
-avionics_arinc429tx = CStruct("avionics_arinc429tx", "=Bxxx", ("flags"))
-avionics_arinc717rx = CStruct("avionics_arinc717rx", "=Bxxx", ("flags"))
-avionics_arinc717tx = CStruct("avionics_arinc717tx", "=Bxxx", ("flags"))
-
-# =============================================================================
+class avionics_arinc429tx(ctypes.Structure):
+    _fields_ = [
+            ('flags', ctypes.c_uint8),
+            ('padding', ctypes.c_uint8 * 3),
+    ]
 
 
-def set_avionics_config(device, set_packet):
-    with socket.socket(
-            socket.AF_NETLINK, socket.SOCK_RAW, socket.NETLINK_ROUTE
-    ) as sock:
-        sock.bind((os.getpid(), 0))
-
-        index = get_index(device)
-
-        kind = b"avionics\x00\x00\x00\x00"
-
-        info_data = rattr.pack(
-                len(rattr) + len(set_packet), IFLA_INFO_DATA
-        ) + set_packet
-
-        info_kind = rattr.pack(len(rattr) + len(kind), IFLA_INFO_KIND) + kind
-
-        command = rattr.pack(
-                len(rattr) + len(info_kind) + len(info_data), IFLA_LINKINFO
-        ) + info_kind + info_data
-
-        msg = (
-                nlmsghdr.pack(
-                        len(nlmsghdr) + len(ifinfomsg) + len(command),
-                        RTM_NEWLINK, NLM_F_REQUEST | NLM_F_ACK, 0, 0
-                ) + ifinfomsg.pack(0, 0, index, IFF_UP, IFF_UP) + command
-        )
-
-        sock.send(msg)
-
-        while True:
-            msg = sock.recv(65535)
-            msg_len = len(msg)
-
-            msg, msghdr = nlmsghdr.consume(msg)
-
-            if msghdr.nlmsg_len != msg_len:
-                print(f"Message truncated! {msghdr.nlmsg_len} != {len(msg)}")
-                return
-
-            if msghdr.nlmsg_type == NLMSG_ERROR:
-                msg, msgerr = nlmsgerr.consume(msg)
-                msg, msgerrhdr = nlmsghdr.consume(msg)
-                if msgerr.error:
-                    print(f"Error: {msgerr} from {msgerrhdr}")
-                return
+# =====================================
 
 
-def set_arinc429tx(device, flags):
-    set_avionics_config(
-            device,
-            rattr.pack(
-                    len(rattr) + len(avionics_arinc429tx),
-                    IFLA_AVIONICS_ARINC429TX
-            ) + avionics_arinc429tx.pack(flags)
+def net_device_get_index(sock, ifname):
+    # get device index from ifname using ioctl call
+    idx, = struct.unpack(
+            "16xi",
+            fcntl.ioctl(
+                    sock, SIOCGIFINDEX,
+                    struct.pack("16si", ifname.encode(), 0)
+            )
     )
 
-
-def set_arinc429rx(
-        device, flags, priority_labels=bytes(3), label_filters=bytes(32)
-):
-    set_avionics_config(
-            device,
-            rattr.pack(
-                    len(rattr) + len(avionics_arinc429rx),
-                    IFLA_AVIONICS_ARINC429RX
-            ) +
-            avionics_arinc429rx.pack(flags, priority_labels, label_filters)
-    )
+    return idx
 
 
-def set_arinc717tx(device, flags):
-    set_avionics_config(
-            device,
-            rattr.pack(
-                    len(rattr) + len(avionics_arinc717tx),
-                    IFLA_AVIONICS_ARINC717TX
-            ) + avionics_arinc717tx.pack(flags)
-    )
-
-
-def set_arinc717rx(device, flags):
-    set_avionics_config(
-            device,
-            rattr.pack(
-                    len(rattr) + len(avionics_arinc717rx),
-                    IFLA_AVIONICS_ARINC717RX
-            ) + avionics_arinc717rx.pack(flags)
-    )
-
-
-# =============================================================================
-
-
-def parse_rtattr(msg):
+def _parse_rtattr(message):
     attrs = {}
-    while msg:
-        msg, msgrattr = rattr.consume(msg)
+    while message:
+        rta = rtattr.from_buffer_copy(message[:ctypes.sizeof(rtattr)])
+        message = message[ctypes.sizeof(rtattr):]
 
-        if msgrattr.rta_len < 4:
-            print(f"Invalid rta length {msgrattr.rta_len}")
-            break
+        if rta.rta_len < 4:
+            print(f"Error: Invalid rta length {rta.rta_len}")
+            exit(1)
 
-        increment = ((msgrattr.rta_len + 4 - 1) & ~(4 - 1)) - len(rattr)
+        increment = ((rta.rta_len + 4 - 1) & ~(4 - 1)) - ctypes.sizeof(rtattr)
 
-        attrs[msgrattr.rta_type] = msg[:msgrattr.rta_len - len(rattr)]
+        attrs[rta.rta_type] = message[:rta.rta_len - ctypes.sizeof(rtattr)]
 
-        msg = msg[increment:]
+        message = message[increment:]
 
     return attrs
 
 
-def getlink(device):
+def link_data_from_msg(message):
+    link_attrs = _parse_rtattr(message)
+    link_info = _parse_rtattr(link_attrs[IFLA_LINKINFO])
+    link_data = _parse_rtattr(link_info[IFLA_INFO_DATA])
+
+    return link_data
+
+
+def get_device_config(ifname):
     with socket.socket(
             socket.AF_NETLINK, socket.SOCK_RAW, socket.NETLINK_ROUTE
-    ) as sock:
-        sock.bind((os.getpid(), 0))
+    ) as sk:
+        sk.bind((os.getpid(), 0))
 
-        index = get_index(device)
+        device_index = net_device_get_index(sk, ifname)
 
-        msg = (
-                nlmsghdr.pack(
-                        len(nlmsghdr) + len(ifinfomsg), RTM_GETLINK,
-                        NLM_F_REQUEST, 0, 0
-                ) + ifinfomsg.pack(socket.AF_PACKET, 0, index, 0, 0)
+        header = nlmsghdr(
+                ctypes.sizeof(nlmsghdr) + ctypes.sizeof(ifinfomsg),
+                RTM_GETLINK, NLM_F_REQUEST, 0, os.getpid()
         )
 
-        sock.send(msg)
+        packet = ifinfomsg(socket.AF_PACKET, 0, 0, device_index, 0, 0)
 
-        while True:
-            msg = sock.recv(65535)
-            msg_len = len(msg)
+        sk.send(bytes(header) + bytes(packet))
+        msg = sk.recv(65535)
 
-            msg, msghdr = nlmsghdr.consume(msg)
+        header = nlmsghdr.from_buffer_copy(msg[:ctypes.sizeof(nlmsghdr)])
 
-            if msghdr.nlmsg_len != msg_len:
-                print(f"Message truncated! {msghdr.nlmsg_len} != {len(msg)}")
-                return
+        if (header.nlmsg_len != len(msg)):
+            print(
+                    "Error: incorrect message"
+                    f" length {header.nlmsg_len}, {len(msg)}"
+            )
+            exit(1)
 
-            if msghdr.nlmsg_type != RTM_NEWLINK:
-                print(f"Unexpected message type {msghdr.nlmsg_type}")
-                return
+        msg = msg[ctypes.sizeof(nlmsghdr):]
 
-            msg, msgifinfo = ifinfomsg.consume(msg)
+        if header.nlmsg_type != RTM_NEWLINK:
+            print(f"Error: Unexpected message type {header.nlmsg_type}")
+            exit(1)
 
-            if msgifinfo.ifi_index != index:
-                print(f"Wrong device index: {msgifinfo.ifi_index}")
-                return
+        ifinfo = ifinfomsg.from_buffer_copy(msg[:ctypes.sizeof(ifinfomsg)])
+        msg = msg[ctypes.sizeof(ifinfomsg):]
 
-            return msg
+        if ifinfo.ifi_index != device_index:
+            print(f"Error: Wrong device index: {ifinfo.ifi_index}")
+            exit(1)
+
+        return link_data_from_msg(msg)
 
 
-def get_arinc429tx(device):
-    msg = getlink(device)
+def str_to_bool(string):
+    if string in ("true", "True", "1", "yes", "enable"):
+        return True
+    return False
 
-    link_attrs = parse_rtattr(msg)
-    link_info = parse_rtattr(link_attrs[IFLA_LINKINFO])
-    link_data = parse_rtattr(link_info[IFLA_INFO_DATA])
 
-    rate = avionics_rate.unpack(link_data[IFLA_AVIONICS_RATE])
-    print(f"Rate = {rate.rate_hz} Hz")
+# =====================================
 
-    config = avionics_arinc429tx.unpack(link_data[IFLA_AVIONICS_ARINC429TX])
-    print(config)
+
+def exit_help():
+    print("==== avionics-config-get.py ====")
+    print("Error: invalid command line arguments")
     print(
-            f"Flip Label Bits ="
-            f" {bool(config.flags&AVIONICS_ARINC429TX_FLIP_LABEL_BITS)}"
+            "== requires three command line arguments, device name,"
+            " setting name, setting value"
     )
-    print(f"Self Test = {bool(config.flags&AVIONICS_ARINC429TX_SELF_TEST)}")
-    print(
-            f"Even Parity ="
-            f" {bool(config.flags&AVIONICS_ARINC429TX_EVEN_PARITY)}"
-    )
-    print(f"Set Parity = {bool(config.flags&AVIONICS_ARINC429TX_PARITY_SET)}")
-
-
-def get_arinc429rx(device):
-    msg = getlink(device)
-
-    link_attrs = parse_rtattr(msg)
-    link_info = parse_rtattr(link_attrs[IFLA_LINKINFO])
-    link_data = parse_rtattr(link_info[IFLA_INFO_DATA])
-
-    rate = avionics_rate.unpack(link_data[IFLA_AVIONICS_RATE])
-    print(f"Rate = {rate.rate_hz} Hz")
-
-    config = avionics_arinc429rx.unpack(link_data[IFLA_AVIONICS_ARINC429RX])
-    print(
-            "Flip Label Bits = "
-            f"{bool(config.flags&AVIONICS_ARINC429RX_FLIP_LABEL_BITS)}"
-    )
-    print(f"SD9 Mask = {bool(config.flags&AVIONICS_ARINC429RX_SD9_MASK)}")
-    print(f"SD10 Mask = {bool(config.flags&AVIONICS_ARINC429RX_SD10_MASK)}")
-    print(
-            "SD Mask Enable = "
-            f"{bool(config.flags&AVIONICS_ARINC429RX_SD_MASK_ENABLE)}"
-    )
-    print(
-            "Parity Check Enabled = "
-            f"{bool(config.flags&AVIONICS_ARINC429RX_PARITY_CHECK)}"
-    )
-    print(
-            "Even Parity = "
-            f"{bool(config.flags&AVIONICS_ARINC429RX_EVEN_PARITY)}"
-    )
-    print(
-            "Label Filter Enabled = "
-            f"{bool(config.flags&AVIONICS_ARINC429RX_LABEL_FILTER_ENABLE)}"
-    )
-    print(
-            "Priority Labels Enabled = "
-            f"{bool(config.flags&AVIONICS_ARINC429RX_PRIORITY_LABEL_ENABLE)}"
-    )
-    print(f"Priority Labels = {config.priority_labels}")
-    print(f"Label Filters = {config.label_filters}")
-
-
-def get_arinc717tx(device):
-    msg = getlink(device)
-
-    link_attrs = parse_rtattr(msg)
-    link_info = parse_rtattr(link_attrs[IFLA_LINKINFO])
-    link_data = parse_rtattr(link_info[IFLA_INFO_DATA])
-
-    rate = avionics_rate.unpack(link_data[IFLA_AVIONICS_RATE])
-    print(f"Rate = {rate.rate_hz} Hz")
-
-    config = avionics_arinc717tx.unpack(link_data[IFLA_AVIONICS_ARINC717TX])
-    print(config)
-    print(f"Slew Rate =" f" {int((config.flags&AVIONICS_ARINC717TX_SLEW)>>1)}")
-    print(f"Self Test = {bool(config.flags&AVIONICS_ARINC717TX_SELF_TEST)}")
-
-
-def get_arinc717rx(device):
-    msg = getlink(device)
-
-    link_attrs = parse_rtattr(msg)
-    link_info = parse_rtattr(link_attrs[IFLA_LINKINFO])
-    link_data = parse_rtattr(link_info[IFLA_INFO_DATA])
-
-    rate = avionics_rate.unpack(link_data[IFLA_AVIONICS_RATE])
-    print(f"Rate = {rate.rate_hz} Hz")
-
-    config = avionics_arinc717tx.unpack(link_data[IFLA_AVIONICS_ARINC717RX])
-    print(config)
-    print(f"BPRZ = {bool(config.flags&AVIONICS_ARINC717RX_BPRZ)}")
-    print(f"No Sync = {bool(config.flags&AVIONICS_ARINC717RX_NOSYNC)}")
-    print(f"Soft Sync = {bool(config.flags&AVIONICS_ARINC717RX_SFTSYNC)}")
-
-
-# =============================================================================
-
-
-def test_arinc429tx():
-    print("==> Testing ARINC-429 TX Config <==")
-    set_arinc429tx(
-            "arinc429tx0", 0
-    )
-    get_arinc429tx("arinc429tx0")
-
-
-def test_arinc429rx():
-    print("==> Testing ARINC-429 RX 0 Config <==")
-    set_arinc429rx(
-            "arinc429rx0", 0
-    )
-    get_arinc429rx("arinc429rx0")
-
-    print("==> Testing ARINC-429 RX 1 Config <==")
-    set_arinc429rx(
-            "arinc429rx1", 0
-    )
-    get_arinc429rx("arinc429rx1")
-
-
-def test_arinc717tx():
-    print("==> Testing ARINC-717 TX Config <==")
-    set_arinc717tx("arinc717tx0", AVIONICS_ARINC717TX_SELF_TEST)
-    get_arinc717tx("arinc717tx0")
-
-
-def test_arinc717rx():
-    print("==> Testing ARINC-717 RX Config <==")
-    set_arinc717rx("arinc717rx0", AVIONICS_ARINC717RX_BPRZ)
-    get_arinc717rx("arinc717rx0")
-    # AVIONICS_ARINC717RX_BPRZ = (1 << 0)
-    # AVIONICS_ARINC717RX_NOSYNC = (1 << 1)
-    # AVIONICS_ARINC717RX_SFTSYNC = (1 << 2)
+    print("== example: avionics-config-set.py arinc429rx0 flip-label true")
+    exit(1)
 
 
 if __name__ == "__main__":
 
-    #test_arinc429rx()
-    #test_arinc429tx()
+    if len(sys.argv) != 4:
+        exit_help()
 
-    test_arinc717tx()
-    test_arinc717rx()
+    device_name = sys.argv[1]
+    setting_name = sys.argv[2]
+    setting_value = sys.argv[3]
+
+    print("==== avionics-config-set.py ====")
+    print(
+            f"-- setting config in {device_name}:"
+            f" {setting_name} to {setting_value}"
+    )
+
+    data = get_device_config(device_name)
+
+    if setting_name == "rate-hz":
+        rate = avionics_rate.from_buffer_copy(data[IFLA_AVIONICS_RATE])
+        if rate.rate_hz == int(setting_value):
+            exit(0)
+
+        data = bytes(avionics_rate(int(setting_value)))
+
+    elif IFLA_AVIONICS_ARINC429RX in data:
+        if setting_name == "flip-label":
+            ...
+        elif setting_name == "sd9-mask":
+            ...
+        elif setting_name == "sd10-mask":
+            ...
+        elif setting_name == "sd-mask-enabled":
+            ...
+        elif setting_name == "parity-check-enabled":
+            ...
+        elif setting_name == "even-parity":
+            ...
+        elif setting_name == "filter-enabled":
+            ...
+        elif setting_name == "priority-enabled":
+            ...
+        elif setting_name == "priority-labels":
+            ...
+        elif setting_name == "label-filters":
+            ...
+        else:
+            print(
+                    f"Error: {setting_name} is not a valid"
+                    " setting for an ARINC-429 RX interface"
+            )
+
+    elif IFLA_AVIONICS_ARINC429TX in data:
+        if setting_name == "flip-label":
+            ...
+        elif setting_name == "self-test":
+            ...
+        elif setting_name == "even-parity":
+            ...
+        elif setting_name == "set-parity":
+            ...
+        else:
+            print(
+                    f"Error: {setting_name} is not a valid"
+                    " setting for an ARINC-429 TX interface"
+            )
+
+    elif IFLA_AVIONICS_MIL1553MB in data:
+        print(
+                f"Error: {setting_name} is not a valid"
+                " setting for an MIL-1553 MB interface"
+        )
+
+    print("===============================")
