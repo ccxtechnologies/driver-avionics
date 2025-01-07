9f74a3762a30e1fc205bf2125ed6a1be748e17d7
tamara <tamara@ccxtechnologies.com>
2025-01-07 15:46:17 -0500

    update version

--- driver/devices/hi3593.c
+++ driver/devices/hi3593.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C), 2019-2023 CCX Technologies
+ * Copyright (C), 2019-2025 CCX Technologies
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -34,7 +34,7 @@
 MODULE_DESCRIPTION("HOLT Hi-3593 ARINC-429 Driver");
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Charles Eidsness <charles@ccxtechnologies.com>");
-MODULE_VERSION("1.2.3");
+MODULE_VERSION("1.2.4");
 
 #define HI3593_FIFO_DEPTH	32
 #define HI3593_MAX_DATA		(HI3593_FIFO_DEPTH * 8)
