90d8d9b0e7d1e9a20c768875ca5bcbd95aff7892
Tamara Tardif <tamara@ccxtechnologies.com>
2026-08-11 16:08:55 -0400

    use big endian for consistency with a429

--- driver/devices/hi3717a.c
+++ driver/devices/hi3717a.c
@@ -638,7 +638,8 @@ static int hi3717a_rxfifo_read(struct hi3717a_priv *priv,
 	struct spi_message message;
 	struct spi_transfer *opcodes = priv->opcodes;
 	__u8 rd_cmd[5], buffer[HI3717A_FIFO_DEPTH*5];
-	__u32 vbuffer, val;
+	__u32 vbuffer;
+	__be32 val_be;
 
 	spi_message_init(&message);
 	memset(opcodes, 0, sizeof(*opcodes) * HI3717A_FIFO_DEPTH);
@@ -660,8 +661,8 @@ static int hi3717a_rxfifo_read(struct hi3717a_priv *priv,
 	for(i = 0; i < num_reads; i++) {
 		vbuffer = buffer[i*5+1] + (buffer[i*5+2]<<8) +
 			(buffer[i*5+3]<<16) + (buffer[i*5+4]<<24);
-		val = be32_to_cpu(vbuffer);
-		memcpy(&dest[i*sizeof(__u32)], &val, sizeof(__u32));
+		val_be = cpu_to_be32(vbuffer);
+		memcpy(&dest[i*sizeof(__u32)], &val_be, sizeof(__be32));
 	}
 
 	if(status < 0) {
@@ -731,6 +732,7 @@ static int hi3717a_rxfifo_read_all(struct hi3717a_priv *priv, __u8 *dest)
 	__u8 *tx_buf, *rx_buf;
 	int i, count = 0, err;
 	__u32 val;
+	__be32 val_be;
 
 	/* 32 words * 2 transfers (Status + Data) = 64 transfers */
 	int num_xfers = HI3717A_FIFO_DEPTH * 2;
@@ -793,8 +795,8 @@ static int hi3717a_rxfifo_read_all(struct hi3717a_priv *priv, __u8 *dest)
 		      (rx_buf[i * 7 + 5] << 16) +
 		      (rx_buf[i * 7 + 6] << 24);
 
-		val = be32_to_cpu(val);
-		memcpy(&dest[count * sizeof(__u32)], &val, sizeof(__u32));
+		val_be = cpu_to_be32(val);
+		memcpy(&dest[count * sizeof(__u32)], &val_be, sizeof(__be32));
 		count++;
 	}
 
@@ -914,6 +916,7 @@ static netdev_tx_t hi3717a_tx_start_xmit(struct sk_buff *skb,
 	struct hi3717a_priv *priv;
 	avionics_data *data_hdr;
 	__u16 frame, word_count, word;
+	__be32 value_be;
 	__u32 value;
 	int offset, i;
 
@@ -954,7 +957,8 @@ static netdev_tx_t hi3717a_tx_start_xmit(struct sk_buff *skb,
 	for (i = 0; i < data_hdr->length; i += sizeof(__u32)) {
 		/* ARINC-717 doesn't use the transmit timestamp for anything */
 
-		memcpy(&value, &data_hdr->data[i], sizeof(__u32));
+		memcpy(&value_be, &data_hdr->data[i], sizeof(__be32));
+		value = be32_to_cpu(value_be);
 
 		word = (value&0x0fff0000)>>16;
 		word_count = (value&0x0000fff8)>>3;
